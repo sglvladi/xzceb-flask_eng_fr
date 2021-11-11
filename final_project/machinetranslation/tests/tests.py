c4934afec1298fedac669a53e07d690c2defec3e
sglvladi <sglvladi@gmail.com>
2021-11-11 02:06:27 +0000

    Final changes v2

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -6,6 +6,8 @@ class TestE2F(unittest.TestCase):
     def test1(self):
         # Test Hello returns Bonjour
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
+        # Test Hello does not return Hello
+        self.assertNotEqual(englishToFrench('Hello'), 'Hello')
         # Test None returns empty string
         self.assertEqual(englishToFrench(None), '')
         # Test empty string returns empty string
@@ -16,6 +18,8 @@ class TestF2E(unittest.TestCase):
     def test1(self):
         # Test Bonjour returns Hello
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
+        # Test Bonjour does not return Bonjour
+        self.assertNotEqual(frenchToEnglish('Bonjour'), 'Bonjour')
         # Test None returns empty string
         self.assertEqual(frenchToEnglish(None), '')
         # Test empty string returns empty string
